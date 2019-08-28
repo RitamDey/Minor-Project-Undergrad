ca27e7bca9eed8ba2f944f6f87a8b7daa21c34ca
Ritam Dey <esyray15@gmail.com>
2019-08-28 13:29:57 +0530

    Added working SQL schema and cleaned up the working directory

--- database/schemas/book_schema.sql
+++ database/schemas/book_schema.sql
@@ -1,36 +1,152 @@
-CREATE TABLE author (
-		name VARCHAR(15) PRIMARY KEY
-) ENGINE=InnoDB CHARSET=utf8;
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 28, 2019 at 09:48 AM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-CREATE TABLE publisher (
-		name VARCHAR(20) PRIMARY KEY,
-		address TEXT NOT NULL,
-		contact VARCHAR(10) NOT NULL UNIQUE
-) ENGINE=InnoDB CHARSET=utf8;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Database: `bookstore`
+--
 
-CREATE TABLE series (
-		title VARCHAR(30) PRIMARY KEY,
-		author VARCHAR(15) NOT NULL,
-		publisher VARCHAR(15) NOT NULL,
-		total_books INT(2) DEFAULT 0,
+-- --------------------------------------------------------
 
-		FOREIGN KEY (author) REFERENCES author(name) ON DELETE CASCADE ON UPDATE CASCADE
-		FOREIGN KEY (publisher) REFERNCES publisher(name) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB CHARSET=utf8;
+--
+-- Table structure for table `author`
+--
 
+CREATE TABLE `author` (
+  `name` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE book (
-		isbn INT(13) PRIMARY KEY,
-		name VARCHAR(100) NOT NULL UNIQUE,
-		price FLOAT NOT NULL,
-		book_number INT(2) DEFAULT NULL,
-		series VARCHAR(15) DEFAULT NULL,
-		publisher VARCHAR(15) NOT NULL,
-		author VARCHAR(15) NOT NULL,
+--
+-- Dumping data for table `author`
+--
 
-		FOREIGN KEY (series) REFERENCES series(title) ON DELETE CASCADE ON UPDATE CASCADE,
-		FOREIGN KEY (publisher) REFERENCES publisher(name) ON DELETE CASCADE ON UPDATE CASCADE,
-		FOREIGN KEY (author) REFERENCES author(name) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB CHARSET=utf8;
+INSERT INTO `author` (`name`) VALUES
+('admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book`
+--
+
+CREATE TABLE `book` (
+  `isbn` int(13) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `price` float NOT NULL,
+  `book_number` int(11) DEFAULT NULL,
+  `series` varchar(15) DEFAULT NULL,
+  `publisher` varchar(15) NOT NULL,
+  `author` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `book`
+--
+
+INSERT INTO `book` (`isbn`, `name`, `price`, `book_number`, `series`, `publisher`, `author`) VALUES
+(2147483647, 'test book', 0, NULL, NULL, 'admin pub', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `publisher`
+--
+
+CREATE TABLE `publisher` (
+  `name` varchar(15) NOT NULL,
+  `address` text DEFAULT NULL,
+  `contact` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `publisher`
+--
+
+INSERT INTO `publisher` (`name`, `address`, `contact`) VALUES
+('admin pub', 'localhost', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `series`
+--
+
+CREATE TABLE `series` (
+  `title` varchar(30) NOT NULL,
+  `author` varchar(15) NOT NULL,
+  `publisher` varchar(15) NOT NULL,
+  `total_books` int(11) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `author`
+--
+ALTER TABLE `author`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Indexes for table `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`isbn`),
+  ADD KEY `author` (`author`),
+  ADD KEY `publisher` (`publisher`),
+  ADD KEY `series` (`series`);
+
+--
+-- Indexes for table `publisher`
+--
+ALTER TABLE `publisher`
+  ADD PRIMARY KEY (`name`) USING BTREE;
+
+--
+-- Indexes for table `series`
+--
+ALTER TABLE `series`
+  ADD PRIMARY KEY (`title`),
+  ADD KEY `author` (`author`),
+  ADD KEY `publisher` (`publisher`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `book`
+--
+ALTER TABLE `book`
+  ADD CONSTRAINT `book_ibfk_1` FOREIGN KEY (`author`) REFERENCES `author` (`name`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `book_ibfk_2` FOREIGN KEY (`publisher`) REFERENCES `publisher` (`name`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `book_ibfk_3` FOREIGN KEY (`series`) REFERENCES `series` (`title`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `series`
+--
+ALTER TABLE `series`
+  ADD CONSTRAINT `series_ibfk_1` FOREIGN KEY (`author`) REFERENCES `author` (`name`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `series_ibfk_2` FOREIGN KEY (`publisher`) REFERENCES `publisher` (`name`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
