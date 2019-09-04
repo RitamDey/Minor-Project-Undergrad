c3cee75e5a6e1774f9aea929250051993eb72d74
Saptarshi Bose <saptarshi.bose007@gmail.com>
2019-09-04 18:41:07 +0530

    All .html extention has converted into .php
    Signed-off-by: Saptarshi Bose <saptarshi.bose007@gmail.com>

---
+++ html/contact.php
@@ -0,0 +1,9 @@
+<?php
+   $name=$_POST['name'];
+   $email=$_POST['email'];
+   $massage=$_POST['massage'];
+
+   echo "Name: $name"."<br>";
+   echo "Email: $email"."<br>";
+   echo "Massage: $massage"."<br>";
+?>
