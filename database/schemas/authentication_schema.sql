15cde082f118c5aa6df9439773f959f7d2a6c737
Ritam <esyray15@gmail.com>
2019-09-07 00:43:59 +0530

    Initial Schema for bill and users
    Signed-off-by: Ritam <esyray15@gmail.com>

---
+++ database/schemas/authentication_schema.sql
@@ -0,0 +1,160 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Sep 06, 2019 at 09:12 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `authentication`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bill`
+--
+-- Creation: Sep 06, 2019 at 07:00 PM
+--
+
+CREATE TABLE `bill` (
+  `id` bigint(20) NOT NULL,
+  `customer` bigint(20) NOT NULL,
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- RELATIONSHIPS FOR TABLE `bill`:
+--   `customer`
+--       `customer` -> `id`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books_bill`
+--
+-- Creation: Sep 06, 2019 at 07:02 PM
+--
+
+CREATE TABLE `books_bill` (
+  `id` bigint(20) NOT NULL,
+  `book` bigint(13) NOT NULL,
+  `quantity` int(11) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- RELATIONSHIPS FOR TABLE `books_bill`:
+--   `book`
+--       `book` -> `isbn`
+--   `id`
+--       `bill` -> `id`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+-- Creation: Sep 06, 2019 at 05:21 PM
+--
+
+CREATE TABLE `customer` (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `dob` date NOT NULL,
+  `phone` bigint(10) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `address` varchar(500) NOT NULL,
+  `state` varchar(5) NOT NULL,
+  `pin` bigint(7) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `picture` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- MIME TYPES FOR TABLE `customer`:
+--   `picture`
+--       `Image_JPEG`
+--
+
+--
+-- RELATIONSHIPS FOR TABLE `customer`:
+--
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bill`
+--
+ALTER TABLE `bill`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `customer` (`customer`);
+
+--
+-- Indexes for table `books_bill`
+--
+ALTER TABLE `books_bill`
+  ADD KEY `id` (`id`),
+  ADD KEY `book` (`book`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `phone` (`phone`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bill`
+--
+ALTER TABLE `bill`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bill`
+--
+ALTER TABLE `bill`
+  ADD CONSTRAINT `bill_ibfk_1` FOREIGN KEY (`customer`) REFERENCES `customer` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `books_bill`
+--
+ALTER TABLE `books_bill`
+  ADD CONSTRAINT `books_bill_ibfk_1` FOREIGN KEY (`book`) REFERENCES `bookstore`.`book` (`isbn`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `books_bill_ibfk_2` FOREIGN KEY (`id`) REFERENCES `bill` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
