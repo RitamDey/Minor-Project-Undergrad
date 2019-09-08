85a1384d239728bc707669bfe42edc7eeba7be5b
Ritam <esyray15@gmail.com>
2019-09-08 22:27:51 +0530

    Fix url and CSS code style
    Signed-off-by: Ritam <esyray15@gmail.com>

---
+++ lib/sanitizers_validators.php
@@ -0,0 +1,9 @@
+<?php
+function sanitizeName($name) {
+    /**
+     * Sanitizes the supplied tag from the $_GET get request variables
+     * @var $name string := The name requested by the user.
+     */
+    return $name;
+}
+?>
