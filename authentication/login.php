f300d5b610c4c9d3f151baade3a8aced354c863e
Ritam Dey <deyritam27031999@gmail.com>
2019-10-22 03:10:25 +0530

    Added documentation to authentication/login.php
    Signed-off-by: Ritam <esyray15@gmail.com>

--- authentication/login.php
+++ authentication/login.php
@@ -14,29 +14,36 @@
         // Login and redirection logic
         $connection = new mysqli("localhost", "bookstore", "bookstore", "authentication");
 
+        // Get the email and password from the POST data
         $email = sanitizeEmail($_POST["email"]);
         $password = sanitizePassword($_POST["password"]);
-        $query = "SELECT id,password FROM customer WHERE email = '{$email}'";
 
+        // Try and get the user's password and id from the database
+        $query = "SELECT id,password FROM customer WHERE email = '{$email}'";
         $user = $connection->query($query);
 
         if ($user !== false && $user->num_rows < 1) {
+            // Redirect to /authentication/login.php if the data provided is not true.
             header("Location: /authentication/login.php", true, 302);
         }
 
         $user = $user->fetch_assoc();
         
         if (!password_verify($password, $user["password"])) {
+             // Redirect to /authentication/login.php if the password provided is not correct.
             header("Location: /authentication/login.php", true, 302);
         }
 
+        // Start the session and create a session id
         session_start();
 
-        $sess_id = session_id();
-        $query = "INSERT INTO user_sessions (session,user,started) VALUES ('{$sess_id}',{$user["id"]},CURRENT_TIMESTAMP)";
+        $sess_id = session_id();  # Get the created session id
 
+        // Insert the newly created session into the database to persist
+        $query = "INSERT INTO user_sessions (session,user,started) VALUES ('{$sess_id}',{$user["id"]},CURRENT_TIMESTAMP)";
         $connection->query($query);
 
+        // Redirect to the /authentication/profile.php after the process is done
         header("Location: /authentication/profile.php", true, 302);
     }
 ?>
