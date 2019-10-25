4cc26894bd3420d7b7b5cfe105e5b9aafcc28711
Ritam Dey <deyritam27031999@gmail.com>
2019-10-25 23:18:59 +0530

    Fixed Up /contact.php
    Signed-off-by: Ritam <esyray15@gmail.com>

--- contact.php
+++ contact.php
@@ -1,5 +1,4 @@
 <?php
-require_once "templates/header.html.php";
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $name = $_POST["name"];
     $email = $_POST["email"];
@@ -13,22 +12,18 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Book Store Template, Free CSS Template, CSS Website Layout</title>
-<meta name="keywords" content="Book Store Template, Free CSS Template, CSS Website Layout, CSS, HTML" />
-<meta name="description" content="Book Store Template, Free CSS Template, Download CSS Website" />
-<link href="../assets/css/contact.css" rel="stylesheet" type="text/css" />
+<title>Contact Us -- Bookstore Inc</title>
+<link href="/assets/css/contact.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
-<!--  Free CSS Templates from www.templatemo.com -->
 <div id="container">
 	<div id="menu">
     	<ul>
             <li><a href="/" >Home</a></li>
             <li><a href="../templates/subpage.html">Search</a></li>
-            <li><a href="subpage.html">Books</a></li>            
+            <li><a href="#">Books</a></li>            
             <li><a href="/index.php?sort=new-releases">New Releases</a></li>  
             <li><a href="/about.php">About Us</a></li> 
             <li><a href="/contact.php">Contact Us</a></li>
@@ -44,10 +39,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <div id="content">
     	
         <div id="content_left">
-        	
-            
-            
-        </div> <!-- end of content left -->
+       </div> <!-- end of content left -->
 
         
         <div class="contact">
@@ -60,9 +52,9 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             <P>Massage Body</P><br>
             <textarea wrap="soft" cols="30" rows="10" placeholder="Enter Your Message" name="massage" required></textarea><br><br>
     
-           <br> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp<input type="submit" value="Submit" name="submit"><br><br>
+           <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="submit" value="Submit" name="submit"><br><br>
            <p>Help line-1800 000 123</p><br>
-           <p>Email Us: info@abc.com</p>
+           <p>Email Us: info@bookstore.inc</p>
             </form>
         </div>
         </div> <!-- end of content -->
@@ -72,13 +64,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 	       <a href="/index.php">Home</a> | <a href="subpage.html">Search</a> | <a href="subpage.html">Books</a> | <a href="/index.php?sort=new-releases">New Releases</a> | <a href="/about.php">FAQs</a> | <a href="/contact.php">Contact Us</a><br />
         Copyright © 2019 <a href="#"><strong>Project Bookstore</strong></a>
     </div> 
-    <!-- end of footer -->
-<!--  Free CSS Template www.templatemo.com -->
-</div> <!-- end of container -->
+</div>
 </body>
 </html>
-
-
-<?php
-require_once "templates/footer.html.php";
-?>
