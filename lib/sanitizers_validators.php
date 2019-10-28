555dfda16972e74904e4d3ee8f1acfc0bf43d11d
Ritam Dey <deyritam27031999@gmail.com>
2019-10-28 14:11:23 +0530

    FIX BUG: Session not correctly resumed due to error in /lib/sanitizers_validators.php
    Signed-off-by: Ritam <esyray15@gmail.com>
    
    Author:    Ritam Dey <deyritam27031999@gmail.com>
    Changes to be committed:
    	modified:   lib/sanitizers_validators.php
    A error in lib/sanitizers_validators.php occured due to calculation of timediff in SQL. This caused problems in PHP as DateTime can't create object when hh > 24. To workaround, now the started time is fetched as-is from database and the calculation of timediff is now handled in  PHP code. Also the session lifetime is now 5 days, instead of 15 days

--- lib/sanitizers_validators.php
+++ lib/sanitizers_validators.php
@@ -36,12 +36,16 @@ function sanitizePassword(string $password): string {
 
 // The argument to the function is a session id, or can be null if there is no current PHPSESSID defined
 function checkActiveSession(?string $session_id): bool {
+    /**
+    * Gets a session id and checks if the session is active for 5 days or less
+    * If it's active for more than five days, then logout from the store
+    **/
     if ($session_id === null)
         return false;
     
     $connection = new mysqli("localhost", "bookstore", "bookstore", "authentication");
     // If a session is present, it would return the active time in hh:mm:ss 
-    $query = "SELECT TIMEDIFF(NOW(), started) AS timedelta FROM user_sessions WHERE session = '{$session_id}'";
+    $query = "SELECT started  FROM user_sessions WHERE session = '{$session_id}'";
     $result = $connection->query($query);
 
     if ($result->num_rows < 1)
@@ -49,11 +53,18 @@ function checkActiveSession(?string $session_id): bool {
     
     $result = $result->fetch_assoc();
 
-    // Convert the returned time difference to UNIX timestamp.
-    $time_active = new DateTime($result["timedelta"]);
-    $fifteen_days = new DateTime("15 days");
+    /**
+     * Workaround for the DateInterval not being comparable.
+     * Converts the session started time, fetched from database, to PHP DateTime, they are comparable
+     * Get a DateTime object for the current time.
+     * Create a new DateTime object that is 5 days ahead of the date when the session was started.
+     * Now compare if the current date is less than five days interval, return true if so.
+    **/
+    $time_started = new DateTime($result["started"]);
+    $time_now = new DateTime();
+    $five_days = $time_started->add(DateInterval::createFromDateString("5 days"));
 
-    if ($time_active < $fifteen_days)
+    if ($time_now < $five_days)
         return true;
     else {
         $query = "DELETE FROM user_sessions WHERE session='{$session_id}";
