c2d2f1d8c648a63edd0269af4c2d9e3f1220d83c
Ritam Dey <deyritam27031999@gmail.com>
2019-10-31 12:32:52 +0530

    Using prepared statment to prevent probable SQLI
    Signed-off-by: Ritam <esyray15@gmail.com>

--- authentication/logout.php
+++ authentication/logout.php
@@ -7,9 +7,14 @@
         header("Location: /", true, 302);
     }
 
-    $query = "DELETE FROM user_sessions WHERE session = '{$_COOKIE["PHPSESSID"]}'";
-    $connection->query($query);
+    // Since the PHPSESSID comes directly from the user's control, it's better treat it as a gateway to possible SQL Injection attacks
+    // So we are using mysqli prepared statments that provides a rudimentary protection against SQL attacks by escaping the giving data using the format specifier passed to it during `bind_param` call
+    $query = $connection->prepare("DELETE FROM user_sessions WHERE session=?");
+    $session = $_COOKIE["PHPSESSID"];
+    $query->bind_param("s", $session);  // Use the string format specifer
+    $query->execute();
 
     session_destroy();
 
+    $query->close();
     header("Location: /", true, 302);
