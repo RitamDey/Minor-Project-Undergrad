9338f1ef8d505b10e32c3e5f913fb6cf101cee2a
Ritam Dey <deyritam27031999@gmail.com>
2019-11-05 14:02:20 +0530

    WIP: /cart.php /buy.php
    Signed-off-by: Ritam <esyray15@gmail.com>

--- buy.php
+++ buy.php
@@ -2,7 +2,6 @@
     $TITLE = "Thank You -- Bookstore Inc";
     require_once "templates/header.html.php";
 
-// Parse the cart COOKIE and insert into the database
 
     require_once "template/footer.html.php";
 ?>
--- cart.php
+++ cart.php
@@ -1,40 +1,71 @@
 <?php
-    $TITLE = "Order Summary -- Bookstore Inc";
+    require_once "lib/sanitizers_validators.php";
+    $TITLE = "Purchase preview -- Bookstore Inc";
     require_once "templates/header.html.php";
-    $connection->select_db("bookstore");
+    $connection->select_db("authentication");
 
-    $books = explode(",", $_COOKIE["cart"]);
-    $book = null;
-    $name = null;
+    if (checkActiveSession($_COOKIE["PHPSESSID"] === false)) {
+        header("Location: /authentication/login.php", true, 302);
+    }
+
+    $session = $_COOKIE["PHPSESSID"];
+    $user_id = null;
+
+    // Using prepared statments as a basic against SQL Injection
+    $user_query = $connection->prepare("SELECT user FROM user_sessions WHERE session=?");
+    $user_query->bind_param("s", $session);
+    $user_query->bind_result($user_id);
+    $user_query->execute();
+    $user_query->fetch();
+    $user_query->close();
+
+    $connection->select_db("sales");
+    
+    /**
+     * It is always guaranteed that the cart's ID will be equals to that of user's ID.
+     * But we need to make sure that the cart actually exists in the database.
+     * To make sure, run a query. If the returned rows are less than 1, create the cart
+    **/
+    $cart_exists = "SELECT cart_id FROM shopcart WHERE user={$user_id}";
+
+    if ($connection->query($cart_exists)->num_rows < 1) {
+        // No user cart exists. Create one.
+        $connection->query("INSERT INTO shopcart (user, cart_id) VALUES ({$user_id}, {$user_id})");
+    }
+    $cart_id = $user_id;
+
+    // Get the books in the cart.
+    $cart_books = "SELECT book,quantity FROM shopcart_items WHERE cart={$cart_id}";
+    $result = $connection->query($cart_books);
+    $isbn = null;
+    $book_name = null;
     $price = null;
-
-    $query = $connection->prepare("SELECT name,price FROM book WHERE isbn=?");
-    $query->bind_param("i", $book);
-    $query->bind_result($name, $price);
- 
-    foreach($books as $book) {
-        if (!$book)
-            continue;
-
-        $query->execute();
-        $book = $query->fetch();;
+    $book_query = $connection->prepare("SELECT name,price FROM bookstore.book WHERE isbn=?");
+    $book_query->bind_param("s", $isbn);
+    $book_query->bind_result($book_name, $price);
 ?>
-
 <table>
     <tr>
-        <th> Book Name </th>
-        <th> Price </th>
+        <th> Book </th>
         <th> Quantity </th>
+        <th> Price </th>
     </tr>
 <?php
-    echo "<tr>";
-    echo "<td>{$name}</td>";
-    echo "<td>{$price}</td>";
-    echo "</tr>";
+    while ($item = $result->fetch_assoc()) {
+        $isbn = $item["book"];
+        $quantity = $item["quantity"];
+        $book_query->execute();
+        $book_query->fetch();
+
+        echo "<tr>";
+        echo "<td>{$book_name}</td>";
+        echo "<td>{$quantity}</td>";
+        echo "<td>{$price}</td>";
+        echo "</tr>";
+    }
 ?>
 </table>
-<button><a href="/buy.php">Buy Now</a></button>
 <?php
-    }
+    $book_query->close();
     require_once "templates/footer.html.php";
 ?>
