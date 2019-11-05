393f1c2d991bb8f7c5f6c02ba898be21aa112886
Ritam Dey <deyritam27031999@gmail.com>
2019-11-05 12:13:45 +0530

    Updated the About Us section of the code
    Signed-off-by: Ritam <esyray15@gmail.com>

--- about.php
+++ about.php
@@ -8,17 +8,11 @@ require_once "templates/header.html.php";
    <h1>About Jain Book Agency</h1>
 
    <p>
-         
-      In the beginning of 1935 late Lala Shanti Pershad Jain of Delhi opened a book-shop named JAIN BOOK AGENCY in Connaught Place, New Delhi – a shop that could cater the entire need of businessmen, corporate houses and lawyers. In that time of recession and uncertainty, he bought a place in the then very remote and ignored area called Connaught Place, built and developed by the British (today one of the top ten market places in the world). It was the birth of a legend; the beginning of JAIN BOOK AGENCY!
-
-      Today, JAIN BOOK AGENCY (JBA) publishes, market and sell books in the field of Indian law, business, economics, politics, education, companies, international trade, business directories, service laws, Indian Railways, current affairs, commercial books - to name a few - in India and abroad through our network of showrooms in Delhi & NCR, mail-order business and user-friendly website which not only has a database of over 50,000 books, but also is convenient for online shopping. You can buy books on-line through credit cards, net banking, bank demand draft, or cash-on-delivery (VPP) basis.
-
-      Incidentally, it is the oldest and the single largest agent for Government and various institutional publications in India. The list of respectful patrons and customers includes the cream of Indian society. JBA has become an ultimate in providing service and variety in law/commercial books and business directories. The choice you get here is rarely found at one place anywhere in India. Many fake people are trying to repeat the style so beware!
-
-      JBA is also the first authorised printer and marketeer of Indian government official PASSPORT Forms and taxation forms, and all other legal forms.
-
-      For the last 70 years, JBA has set examples. It has become a phenomenon! So, step-in, telephone, fax or e-mail your requirements and a prompt reply is already there.
+   We came up with the idea to collaborate each interested bookworms who are eager to learn and explore the world thorough books. Our team of four( Ritam Dey, Saptarshi Bose, Subhangi De, Deep Kamal Das) came up with this idea. This online store is exclusively made for the readers of all genre. this onine site is a currated work of story books and educational books. We welcome the readers cordially, help us in our growth.
    </p>
+<b>
+THIS IS AN EDUCATIONAL PROJECT. NO RIGHTS RESERVED.
+</b>
 </div>
 <link rel="stylesheet" href="/assets/css/image.css">
 <?php
