e83897118bbc6a05eeb9e3a570d196b2b06193c7
Saptarshi Bose <saptarshi.bose007@gmail.com>
2019-11-05 16:16:01 +0530

    Admin View to list all the transacions for the admin
    Signed-off-by: Saptarshi Bose <saptarshi.bose007@gmail.com>
    
    Now /admin/transaction.php lists all the sales done on the website and /admin/transaction.php?id= shows the details of a particular billin question

--- admin/transaction.php
+++ admin/transaction.php
@@ -1,16 +1,15 @@
 <?php
-$connect=mysqli_connect("localhost","root","","saptarshi") or mysqli_error();
+$connect=mysqli_connect("localhost","root","","sales") or mysqli_error();
 
 echo "<br>";
 
-    $result=mysqli_query($connect, "select * from books_bill");
-
+	$result=mysqli_query($connect, "select * from bill");
 ?>
 <title>
-	Trnsaction History
+	Transcation History
 </title>
 <head>
-<link rel="stylesheet" href="../image.css">
+<link rel="stylesheet" href="/admin/assets/image.css">
 <link rel="stylesheet" href="/assets/css/emp.css">
 	<h1>Transacion Hisory&nbsp</h1>
 	<hr size="5" color="red"></hr>
@@ -41,29 +40,71 @@ echo "<br>";
 			</ul>
 			
 		</div>
-    <br>
+	<br>
+<?php
+	if (!isset($_GET['id'])) {
+?>
   <div class="loginbox">
     <table>
             <th><P>Bill ID</P></th>
-            <th><p>Book ID</P></th>
-            <th><P>Quantity</P></th>
+            <th><p>Billed To</P></th>
+            <th><P>Billed Date</P></th>
            
             
 <?php
-while($row=mysqli_fetch_assoc($result))
-{   
-    echo "<tr>";
-    
-    
-    echo "<td>"."<p>".$row['bill_id']."</P>"."</td>";
-    echo "<td>"."<p>".$row['book']."</P>"."</td>";
-    echo "<td>"."<p>".$row['quantity']."</P>"."</td>";
-    
-    
-    echo "</tr>";
-    
-}
+	while($row=mysqli_fetch_assoc($result)) {   
+			echo "<tr>";
+			
+			$user_name = "SELECT name FROM authentication.customer WHERE id={$row['billed_to']}";
+			$user = $connect->query($user_name)->fetch_assoc();
 
+			echo "<td>"."<a href='/admin/transaction.php?id={$row['id']}'><p>".$row['id']."</p></a>"."</td>";
+			echo "<td>"."<p>".$user['name']."</P>"."</td>";
+			echo "<td>"."<p>".$row['time']."</P>"."</td>";
+			
+			
+			echo "</tr>";
+		}
+	} else {
+		$bill_query = "SELECT * FROM sales.books_bill WHERE bill_id={$_GET["id"]}";
+		$result = $connect->query($bill_query);
+		
+		if ($connect->errno) {
+			echo "DATABASE ERROR: " . $connect->error;
+			die();
+		} else {
+			$isbn = null;
+			$name = null;
+			$price = null;
+
+			$statment = $connect->prepare("SELECT name,price FROM bookstore.book WHERE isbn=?");
+			$statment->bind_param("s", $isbn);
+			$statment->bind_result($name, $price);
+			
+
+?>
+	<div class="loginbox">
+		<table>
+			<th><p>ISBN</p></th>
+			<th><p>Book Name</p></th>
+			<th><p>Quantity</p></th>
+			<th><p>Price</p></th>
+<?
+			while ($item = $result->fetch_assoc()) {
+				echo "<tr>";
+
+				$isbn = $item["book"];
+				$statment->execute();
+				$statment->fetch();
+				echo "<td><p>{$item["book"]}</p></td>";
+				echo "<td><p>{$name}</p></td>";
+				echo "<td><p>{$item["quantity"]}</p></td>";
+				echo "<td><p>{$price}</p></td>";
+
+				echo "</tr>";
+			}
+		}
+	}
 ?>
 
 </table>
