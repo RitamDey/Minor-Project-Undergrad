aee1830cbb89631eabf3e7f200c14f51b6b5135e
Ritam Dey <deyritam27031999@gmail.com>
2019-11-14 00:20:34 +0530

    Simple query statment to set global logging of statments
    Signed-off-by: Ritam <esyray15@gmail.com>

--- templates/header.html.php
+++ templates/header.html.php
@@ -5,6 +5,7 @@ $connection = new mysqli("localhost", "bookstore", "bookstore");
 
 if ($connection->get_connection_stats() === false) {
     die($connection->connect_error);
+    $connection->query("SET global global_log = 1;");
 }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
