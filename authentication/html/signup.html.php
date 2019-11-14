dbeca0ebedaf5ed7c8105a45f9e3520ab7e1f972
Ritam Dey <deyritam27031999@gmail.com>
2019-11-14 21:25:26 +0530

    Added templates in signup.html.php and update.html.php
    Signed-off-by: Ritam <esyray15@gmail.com>

--- authentication/html/signup.html.php
+++ authentication/html/signup.html.php
@@ -1,58 +1,22 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Sign Up -- Bookstore Inc</title>
+<?php require_once "templates/header.html.php"; ?>   
+<div id="content">
 <link href="/assets/css/signup1.css" rel="stylesheet" type="text/css" />
-</head>
-<body>
-<div id="container">
-	<div id="menu">
-    	<ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="subpage.html">Search</a></li>
-            <li><a href="subpage.html">Books</a></li>            
-            <li><a href="/index.php?sort=new-releases">New Releases</a></li>  
-            <li><a href="/about.php">About Us</a></li> 
-            <li><a href="/contact.php">Contact Us</a></li>
-            <li><a href="/authentication/login.php">Login</a></li>
-            <li><a href="#" class="current">Signup</a></li>
-    	</ul>
-    </div> <!-- end of menu -->
-    
-    <div id="header">
-        
-    </div> <!-- end of header -->
-    
-    <div id="content">
-    	
-        <div id="content_left">
-        </div> <!-- end of content left -->
-
-        
-        <div class="signup">
-            <h1 align="center">Quick Signup</h1><br>
-            <form name="signup" method="POST" action="<?php echo $_SERVER["PHP_SELF"] // PHP_SELF refers to the current page?>">
-               <label for="email">Email</p>
-                <input type="email" name="email" id="email" placeholder="Enter your email address" required><br>
-                <label for="password">Password</label>
-                <input type="password" id="password" name="password" placeholder="Create a password" required onfocusout="validate_password()"><br>
-                <label for="cpassword">Confirm password</p>
-                <input type="password" id="cpassword" placeholder="Confirm your password" required onfocusout="check_passwords()"><br>
-               
-               <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="submit" name="submit" value="submit"> <br>
-               <br>
-           </form>
-        </div>
-        <br>Already have an account&nbsp;<a href="/authentication/login.php">Login</a>
-        </div> <!-- end of content -->
-   
-    <div id="footer">
-    
-	       <a href="/index.php">Home</a> | <a href="subpage.html">Search</a> | <a href="subpage.html">Books</a> | <a href="/index.php?sort=new-releases">New Releases</a> | <a href="/about.php">FAQs</a> | <a href="/contact.php">Contact Us</a><br />
-        Copyright © 2019 <a href="#"><strong>Project Bookstore</strong></a>
+    <div class="signup">
+        <h1 align="center">Quick Signup</h1><br>
+        <form name="signup" method="POST" action="<?php echo $_SERVER["PHP_SELF"] // PHP_SELF refers to the current page?>">
+            <label for="email">Email</p>
+            <input type="email" name="email" id="email" placeholder="Enter your email address" required><br>
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" placeholder="Create a password" required onfocusout="validate_password()"><br>
+            <label for="cpassword">Confirm password</p>
+            <input type="password" id="cpassword" placeholder="Confirm your password" required onfocusout="check_passwords()"><br>
+           
+           <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="submit" name="submit" value="submit"> <br>
+           <br>
+       </form>
+       <br>Already have an account&nbsp;<a href="/authentication/login.php">Login</a>
     </div>
-</div> <!-- end of container -->
+    </div> <!-- end of content -->
 <script>
     function validate_password() {
         /**
@@ -74,6 +38,4 @@
        }
     }
 </script>
-
-</body>
-</html>
+<?php require_once "templates/footer.html.php"; ?>
