a795e30876dbd4810e38a9f5baae346166e4b294
Ritam Dey <deyritam27031999@gmail.com>
2019-11-14 21:39:46 +0530

    Fix up links in templates/footer.html/php and updated rights
    Signed-off-by: Ritam <esyray15@gmail.com>

--- templates/footer.html.php
+++ templates/footer.html.php
@@ -5,12 +5,11 @@
 ?>
     <div id="footer">
 
-        <a href="/index.php">Home</a> | <a href="subpage.html">Search</a> | <a href="subpage.html">Books</a> | <a href="/index.php?new-releases">New Releases</a> | <a href="/about.php">FAQs</a> | <a href="/contact.php">Contact Us</a><br />
-        Copyright © 2019 <a href="/"><strong>Bookstore Inc</strong></a>
+        <a href="/index.php">Home</a> | <a href="/index.php?new-releases">New Releases</a> | <a href="/about.php">FAQs</a> | <a href="/contact.php">Contact Us</a> | <a href="https://www.gitlab.com/sTux/minor-project-undergrad/">Source code</a><br />
+        No rights reserved © 2019 <a href="/"><strong>Bookstore Inc</strong></a>
     </div>
     <!-- end of footer -->
     </div> <!-- end of container -->
-    <script src="https://cdn.jsdelivr.net/npm/js-cookie@beta/dist/js.cookie.min.js"></script>
     <script src="https://kit.fontawesome.com/795f6b8766.js" crossorigin="anonymous"></script>
 </body>
 </html>
