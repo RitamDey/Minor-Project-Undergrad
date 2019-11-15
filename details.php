2b5c2e2eb2d1d4e0768ec21de5c31153955bc13d
Ritam Dey <deyritam27031999@gmail.com>
2019-11-15 06:15:15 +0530

    Formatting the PHP
    Signed-off-by: Ritam <esyray15@gmail.com>

--- details.php
+++ details.php
@@ -1,24 +1,24 @@
 <?php
-$TITLE = "Details -- Bookstore Inc";
-require_once "templates/header.html.php";
-require_once "lib/sanitizers_validators.php";
-$connection->select_db("bookstore");
+    $TITLE = "Details -- Bookstore Inc";
+    require_once "templates/header.html.php";
+    require_once "lib/sanitizers_validators.php";
+    $connection->select_db("bookstore");
 
-$isbn = validateISBN($_GET["isbn"]);
-$details_query = "SELECT isbn,name,detail,date_published,price,book_number,series,publisher,author,picture,total_pages FROM bookstore.book WHERE isbn = " . $isbn;
+    $isbn = validateISBN($_GET["isbn"]);
+    $details_query = "SELECT isbn,name,detail,date_published,price,book_number,series,publisher,author,picture,total_pages FROM bookstore.book WHERE isbn = " . $isbn;
 
-$bookDetails = $connection->query($details_query);
+    $bookDetails = $connection->query($details_query);
 
-if ($bookDetails === false || $bookDetails->num_rows < 1)
-    // Display a book not found page
-    require "html/book_not_found.html";
+    if ($bookDetails === false || $bookDetails->num_rows < 1)
+        // Display a book not found page
+        require "html/book_not_found.html";
 
-$book = $bookDetails->fetch_assoc();
+    $book = $bookDetails->fetch_assoc();
 
-$tags_query = "SELECT tag FROM is_tagged WHERE isbn = '{$isbn}'";
-$tags = $connection->query($tags_query);
+    $tags_query = "SELECT tag FROM is_tagged WHERE isbn = '{$isbn}'";
+    $tags = $connection->query($tags_query);
 
-// TODO: Implement the validations of picture
+    // TODO: Implement the validations of picture
 ?>
 <div id="content">
         <div id="content_left">
@@ -26,9 +26,9 @@ $tags = $connection->query($tags_query);
             	<h1>Tags</h1>
                 <ul>
                     <?php
-                    while ($tag = $tags->fetch_assoc()) {
-                        echo "<li><a href=\"/tag.php?tag={$tag["tag"]}\">{$tag["tag"]}</a></li>";
-                    }
+                        while ($tag = $tags->fetch_assoc()) {
+                            echo "<li><a href=\"/tag.php?tag={$tag["tag"]}\">{$tag["tag"]}</a></li>";
+                        }
                     ?>
             	</ul>
             </div>
@@ -39,8 +39,8 @@ $tags = $connection->query($tags_query);
             <h1><?php echo $book["name"]; ?></h1>
             <div class="image_panel"><img src="<?php echo $book["picture"]; ?>" alt="Cover" width="100" height="150" /></div>
             <?php
-            if ($book["series"])
-                echo "<h2>Book {$book["book_number"]} of <a href=\"/series.php?series={$book["series"]}\">{$book["series"]}</a></h2>";
+                if ($book["series"])
+                    echo "<h2>Book {$book["book_number"]} of <a href=\"/series.php?series={$book["series"]}\">{$book["series"]}</a></h2>";
             ?>
             <ul>
                 <li>By <a href="/author.php?name=<?php echo $book["author"]; ?>"><?php echo $book["author"]; ?></a></li>
